60a2effded6d7045dfa3ab6550f407dbefb516d9
广骏 <songpeng.sp@alibaba-inc.com>
2016-07-16 16:09:33 +0800

    space-on-style

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -76,7 +76,7 @@ $corpId = isset($_GET['corpId']) && !empty($_GET['corpId']) ? $_GET['corpId'] : 
       <div class="masthead">
         <div class="head-nav">
           <a href="<?php echo site_url();?>" <?php if($this->uri->segment(2) == 'list' || $this->uri->segment(1)=='') echo "class='active'";?> ><span class='nav-tab-icon'>&#xe601;</span><br /><span >发现</span></a>
-          <div <?php if($this->uri->segment(2) == 'detail'){echo "class='active head-nav-item'";} else {echo "class='head-nav-item'";}?> id='J_Share_Button' data-method="biz.util.scan" data-param='{"type":"barCode"}' data-action="share"><span class='nav-tab-icon'>&#xe603;</span><br /><span>漂儿</span></div>
+          <div <?php if($this->uri->segment(2) == 'detail'){echo "class='active head-nav-item'";} else {echo "class='head-nav-item'";}?> id='J_Share_Button' data-method="biz.util.scan" data-param='{"type":"barCode"}' data-action="share"><span class='nav-tab-icon'>&#xe603;</span><br /><span>放漂</span></div>
           <a href="<?php echo site_url('space/items');?>" <?php if($this->uri->segment(1) == 'space') echo "class='active'";?> ><span class='nav-tab-icon'>&#xe602;</span><br /><span>我的</span></a>
         </div>
       </div>
