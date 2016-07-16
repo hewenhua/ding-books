18b7d5883d34b70adcb531d48516861dcebb6d37
广骏 <songpeng.sp@alibaba-inc.com>
2016-07-16 15:13:18 +0800

    space-on-style

--- application/views/space/borrow.php
+++ application/views/space/borrow.php
@@ -1,94 +1,84 @@
 <?php if(empty($more)):?>
 <div class="row">
-  <div class="span9">
     <div class="alert alert-error hide" id="msg-box"></div>
     <?php if(empty($trades)){ ?>
-    <div class="alert alert-info" id="msg-box">你还没有受漂</div>
+    <div class="alert alert-info" id="msg-box">你还没有受求漂</div>
     <?php } ?>
-    <ul class="thumbnails">
+    <div class="book-list">
       <?php 
 endif;
       foreach ($trades as $key => $trade) { ?>
-      <li class="span9">
-        <div class="thumbnail">
-          <div class="row">
-            <a href="" class="span2">
-              <img class="book_image" src="<?php echo $trade['image_url'];?>" >
+        <div class="book-item">
+          <div class="book-intro">
+            <a href="" class="book-img-link">
+              <img class="book-image" src="<?php echo $trade['image_url'];?>" >
             </a>
-            <div class="span6">
+            <div class="book-info">
               <?php
-              $title_anchor = anchor_popup(site_url('share/detail/'.$trade['item_id'] . '?display=1') , $trade['item_title'] );
-              $owner_anchor = anchor_popup(site_url('#') , $trade['owner_name'] );
+              $title_anchor = $trade['item_title'];
+              $owner_anchor = $trade['owner_name'];
               ?>
               <!-- title -->
-              <h4><?php echo $title_anchor;?></h4>
-              <p><?php echo $owner_anchor;?> : <span><?php echo $trade['item_description'];?></span></p>
-              <div id="trade_record">
-              <?php 
-              foreach ($trade['trade_record'] as $key => $record) {?>
-                 <p><?php echo $record['create_time'];?> : 
-                  <?php if($record['op'] == 1){
-                    echo 'You sent ' . $owner_anchor . ' a request for this book .';
-                   }else if($record['op'] == 2){ 
-                    echo 'The onwer accepted your request .';
-                   }else if($record['op'] == 3){ 
-                    echo 'The onwer denied your request .'; 
-                   }else if($record['op'] == 4){ 
-                    echo 'You cancelled the request .'; 
-                   }else if($record['op'] == 5){ 
-                    echo 'The onwer confirm the book is returned .';
-                   }else if($record['op'] == 6){ 
-                    echo 'The onwer confirm the book is lost .';
-                   } ?>
-                 </p>
-              <?php } ?>
-              </div>
-
-              <?php if($trade['trade_status'] == 1){ //accept or deny?>
-                <p>The owner has not responsed yet , you can cancel the request .</p>
-                <p>
-                <button class="btn btn-danger trade_op" trade_op="cancel" trade_id="<?php echo $trade['trade_id'];?>" type="button">Cancel</button>
-                </p>
-              <?php }else if($trade['trade_status'] == 2){?>
-                <p>The owner has agreed to lend you the book.</p>
-                <p>You can contact the owner using infomation below :</p>
-                <div class="alert alert-info">
-                  <p>Cellphone : <?php echo $trade['owner_cellphone'];?></p>
-                  <p>Email : <?php echo $trade['owner_email'];?></p>
-                </div>
-              <?php }else if($trade['trade_status'] == 3){?>
-                <p>The owner has denied your request for this book.</p>
-                <p>Sorry for that .</p>
-              <?php }else if($trade['trade_status'] == 4){?>
-                <p>You have canceled the request for this book.</p>
-              <?php }else if($trade['trade_status'] == 5){?>
-                <p>The owner has confirm the book has been returned .</p>
-                <p>Thanks for using our system.</p>
-              <?php }else if($trade['trade_status'] == 6){?>
-                <p>The owner has confirm the book has been lost .</p>
-                <p>Sorry for that .</p>
-              <?php }else{?>
-                <p>Sytem error.</p>
-              <?php }?>
-
-            </div>
-            <div class="span1">
-
+              <h4 class='book-title'><?php echo $title_anchor;?></h4>
+              <p class='book-desc'><?php echo $trade['item_description'];?></p>
+              <span class='book-owner'>拥有者：<?php echo $owner_anchor;?></span>
             </div>
           </div>
+          <div id="trade_record">
+            <?php
+            foreach ($trade['trade_record'] as $key => $record) {?>
+               <p><?php echo $record['create_time'];?> :
+                <?php if($record['op'] == 1){
+                  echo 'You sent ' . $owner_anchor . ' a request for this book .';
+                 }else if($record['op'] == 2){
+                  echo 'The onwer accepted your request .';
+                 }else if($record['op'] == 3){
+                  echo 'The onwer denied your request .';
+                 }else if($record['op'] == 4){
+                  echo 'You cancelled the request .';
+                 }else if($record['op'] == 5){
+                  echo 'The onwer confirm the book is returned .';
+                 }else if($record['op'] == 6){
+                  echo 'The onwer confirm the book is lost .';
+                 } ?>
+               </p>
+            <?php } ?>
+          </div>
+          <div>
+            <?php if($trade['trade_status'] == 1){ //accept or deny?>
+              <p>The owner has not responsed yet , you can cancel the request .</p>
+              <p>
+              <button class="btn btn-danger trade_op" trade_op="cancel" trade_id="<?php echo $trade['trade_id'];?>" type="button">Cancel</button>
+              </p>
+            <?php }else if($trade['trade_status'] == 2){?>
+              <p>The owner has agreed to lend you the book.</p>
+              <p>You can contact the owner using infomation below :</p>
+              <div class="alert alert-info">
+                <p>Cellphone : <?php echo $trade['owner_cellphone'];?></p>
+                <p>Email : <?php echo $trade['owner_email'];?></p>
+              </div>
+            <?php }else if($trade['trade_status'] == 3){?>
+              <p>The owner has denied your request for this book.</p>
+              <p>Sorry for that .</p>
+            <?php }else if($trade['trade_status'] == 4){?>
+              <p>You have canceled the request for this book.</p>
+            <?php }else if($trade['trade_status'] == 5){?>
+              <p>The owner has confirm the book has been returned .</p>
+              <p>Thanks for using our system.</p>
+            <?php }else if($trade['trade_status'] == 6){?>
+              <p>The owner has confirm the book has been lost .</p>
+              <p>Sorry for that .</p>
+            <?php }else{?>
+              <p>Sytem error.</p>
+            <?php }?>
+          </div>
         </div>
-      </li>
       <?php }
 if(empty($more)):
 ?>
-    </ul>
-    <div class="pagination">
-      <ul>
-      <?php foreach ($link_array as $key => $value) {echo $value;}?>
-      </ul>
     </div>
     
-  </div>
+
 </div>
 
 <script type="text/javascript">
@@ -96,17 +86,4 @@ if(empty($more)):
   updateTrade(post_url);
 </script>
 
- <style type="text/css">
-      .book_image {
-        margin-left: 5px;
-        margin-top: 5px;
-        margin-bottom: 5px;
-
-        width: 100px; 
-        height: 130px;
-      }
-      .share_status{
-        margin-left: 25px;
-      }
-</style>
 <?php endif;?>
