94dcbda0d0dda2e16fbb6dc4fb4445f75997f02e
广骏 <songpeng.sp@alibaba-inc.com>
2016-07-16 16:50:30 +0800

    space-on-style

--- openapi/public/javascripts/demo.js
+++ openapi/public/javascripts/demo.js
@@ -253,13 +253,13 @@ dd.ready(function() {
     });
 
     $('.J_shake').on('click', function() {
-        alert(1);
+        //alert(1);
         dd.device.accelerometer.watchShake({
-            sensitivity: 15, //振动幅度，加速度变化超过这个值后触发shake
-            frequency: 150, //采样间隔(毫秒)，指每隔多长时间对加速度进行一次采样， 然后对比前后变化，判断是否触发shake
-            callbackDelay: 100,
+            sensitivity: 10, //振动幅度，加速度变化超过这个值后触发shake
+            frequency: 100, //采样间隔(毫秒)，指每隔多长时间对加速度进行一次采样， 然后对比前后变化，判断是否触发shake
+            callbackDelay: 150,
             onSuccess: function(result) {
-                alert(2);
+                //alert(2);
                 dd.device.notification.vibrate({
                     duration: 300,
                     onSuccess: function() {
@@ -276,7 +276,7 @@ dd.ready(function() {
                   dataType:'json',
                   timeout: 900,
                   success: function (data, status, xhr) {
-                      alert(3);
+                    // alert(3);
                     logger.i('data: ' + data);
                     var info = JSON.parse(data);
                     if(info.errcode === 0) {
