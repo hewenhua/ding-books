c5b4a14a595272e98ca4fdbb13d9eeccee25c33e
广骏 <songpeng.sp@alibaba-inc.com>
2016-07-16 15:53:12 +0800

    space-on-style

--- application/views/space/shared.php
+++ application/views/space/shared.php
@@ -20,54 +20,54 @@ endif;
               ?>
               <!-- title -->
               <h4><?php echo $trade['item_title'];?></h4>
-              <div id="trade_record">
-                  <?php
-                  foreach ($trade['trade_record'] as $key => $record) {?>
-                     <p><?php echo $record['create_time'];?> :
-                      <?php if($record['op'] == 1){
-                        echo $borrower_anchor .' 向你发起了求漂.';
-                       }else if($record['op'] == 2){
-                        echo '你接受了求漂 .';
-                       }else if($record['op'] == 3){
-                        echo '你拒绝了求漂 .';
-                       }else if($record['op'] == 4){
-                        echo '求漂者取消了请求 .';
-                       }else if($record['op'] == 5){
-                        echo '你确认了书已归还 .';
-                       }else if($record['op'] == 6){
-                        echo '你确认了书已丢失 .';
-                       } ?>
-                     </p>
-                  <?php } ?>
-              </div>
-              <!-- change book -->
-              
-              <?php if($trade['trade_status'] == 1){ //accept or deny?>
-                <p>
-                <button class="btn btn-success trade_op" trade_op="accept" trade_id="<?php echo $trade['trade_id'];?>" type="button">同意</button>
-                <button class="btn btn-danger trade_op" trade_op="deny" trade_id="<?php echo $trade['trade_id'];?>" type="button">拒绝</button>
-                </p>
-              <?php }else if($trade['trade_status'] == 2){?>
-                <p>你接受了 <?php echo $borrower_anchor;?> 的求漂.</p>
-                <p>
-                <button class="btn btn-primary trade_op" trade_op="return" trade_id="<?php echo $trade['trade_id'];?>" type="button">已归还</button>
-                <button class="btn btn-danger trade_op" trade_op="lost" trade_id="<?php echo $trade['trade_id'];?>" type="button">已丢失</button>
-                </p>
-              <?php }else if($trade['trade_status'] == 3){?>
-                <p>你拒绝了 <?php echo $borrower_anchor;?> 的求漂.</p>
-              <?php }else if($trade['trade_status'] == 4){?>
-                <p>求漂者取消了请求.</p>
-              <?php }else if($trade['trade_status'] == 5){?>
-                <p>你已确认书归还.</p>
-                <p>感谢使用闲书漂流.</p>
-              <?php }else if($trade['trade_status'] == 6){?>
-                <p>你已确认书丢失.</p>
-                <p>非常抱歉.</p>
-              <?php }else{?>
-                <p>系统错误.</p>
-              <?php }?>
-
             </div>
+          </div>
+          <div id="trade_record" class='trade-record'>
+            <?php
+            foreach ($trade['trade_record'] as $key => $record) {?>
+               <p><?php echo $record['create_time'];?> :
+                <?php if($record['op'] == 1){
+                  echo $borrower_anchor .' 向你发起了求漂.';
+                 }else if($record['op'] == 2){
+                  echo '你接受了求漂 .';
+                 }else if($record['op'] == 3){
+                  echo '你拒绝了求漂 .';
+                 }else if($record['op'] == 4){
+                  echo '求漂者取消了请求 .';
+                 }else if($record['op'] == 5){
+                  echo '你确认了书已归还 .';
+                 }else if($record['op'] == 6){
+                  echo '你确认了书已丢失 .';
+                 } ?>
+               </p>
+            <?php } ?>
+          </div>
+          <div class='shared-action-area'>
+
+            <?php if($trade['trade_status'] == 1){ //accept or deny?>
+              <div class='shared-actions'>
+                <button class="btn btn-success trade_op shared-action-button" trade_op="accept" trade_id="<?php echo $trade['trade_id'];?>" type="button">同意</button>
+                <button class="btn btn-danger trade_op shared-action-button" trade_op="deny" trade_id="<?php echo $trade['trade_id'];?>" type="button">拒绝</button>
+              </div>
+            <?php }else if($trade['trade_status'] == 2){?>
+              <p>你接受了 <?php echo $borrower_anchor;?> 的求漂.</p>
+              <div class='shared-actions'>
+                <button class="btn btn-primary trade_op shared-action-button" trade_op="return" trade_id="<?php echo $trade['trade_id'];?>" type="button">已归还</button>
+                <button class="btn btn-danger trade_op shared-action-button" trade_op="lost" trade_id="<?php echo $trade['trade_id'];?>" type="button">已丢失</button>
+              </div>
+            <?php }else if($trade['trade_status'] == 3){?>
+              <p>你拒绝了 <?php echo $borrower_anchor;?> 的求漂.</p>
+            <?php }else if($trade['trade_status'] == 4){?>
+              <p>求漂者取消了请求.</p>
+            <?php }else if($trade['trade_status'] == 5){?>
+              <p>你已确认书归还.</p>
+              <p>感谢使用闲书漂流.</p>
+            <?php }else if($trade['trade_status'] == 6){?>
+              <p>你已确认书丢失.</p>
+              <p>非常抱歉.</p>
+            <?php }else{?>
+              <p>系统错误.</p>
+            <?php }?>
           </div>
         </div>
       <?php }
